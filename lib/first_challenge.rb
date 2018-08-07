202ff243ed9389f06cedc9b5829bbd23dfbd22b7
Adam Johnson <adamwjo@gmail.com>
2018-08-07 13:58:07 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          if flavor == "strawberry"
-            attribute.delete("strawberry")
+          attribute.delete_if { |e| e == "strawberry" }
           end
         end
       end
